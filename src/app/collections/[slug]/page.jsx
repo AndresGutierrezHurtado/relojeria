29cc5de292f7ce57ec55a7237bf10babb66a3014
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-02-19 13:30:13 -0500

    feat: add products handler

--- src/app/collections/[slug]/page.jsx
+++ src/app/collections/[slug]/page.jsx
@@ -8,16 +8,27 @@ import ProductCard from "@/components/product";
 
 export default async function Page({ params }) {
     const { slug } = params;
-    const { watches, pages } = await useGetData(`/api/watches?collection=${slug}`);
+    const { watches, pages } = await useGetData(
+        `/api/watches?collection=${slug}`
+    );
 
     return (
         <div className="flex flex-col gap-5 pb-10">
             <h2 className="text-4xl font-bold text-white">Productos:</h2>
+            {watches.length == 0 && (
+                <div className="text-center py-10">
+                    <h2 className="text-xl font-medum">No se encontraron existencias..</h2>
+                    <p>Espera a que el adminstrador publique más productos</p>
+                </div>
+            )}
             <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-10">
                 {watches
                     // .filter((p) => p.product_page == currentPage)
                     .map((product) => (
-                        <ProductCard key={product.product.id} product={product} />
+                        <ProductCard
+                            key={product.product.id}
+                            product={product}
+                        />
                     ))}
             </div>
         </div>
