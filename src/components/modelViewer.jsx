d4f08fb1d2dc3f7fd0f99dc6c084ffd9081f1abc
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-02-22 14:17:57 -0500

    add time animation

--- src/components/modelViewer.jsx
+++ src/components/modelViewer.jsx
@@ -2,28 +2,50 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useState, useEffect, Suspense } from "react";
-import { MeshPhysicalMaterial } from "three";
-
+import * as THREE from "three";
 const Model = () => {
-    const { scene } = useGLTF("/watch.glb");
-    console.log(scene);
+    const { scene } = useGLTF("/scene.glb");
 
-    const metalMaterial = new MeshPhysicalMaterial({
-        color: 0xffffff,
-        metalness: 1,
-        roughness: 0.5,
-        clearcoat: 1,
-        clearcoatRoughness: 0.1,
-        reflectivity: 0.5,
-    });
+    const secondsPointer = scene.getObjectByName("Cylinder013");
+    const minutePointer = scene.getObjectByName("Cylinder016");
+    const hourPointer = scene.getObjectByName("Cylinder017");
+
+    const [time, setTime] = useState(
+        new Date().toLocaleString("en-US", { timeZone: "America/Bogota" })
+    );
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleString("en-US", { timeZone: "America/Bogota" }));
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const hours = parseInt(time.split(" ")[1].split(":")[0]);
+    const minutes = parseInt(time.split(" ")[1].split(":")[1]);
+    const seconds = parseInt(time.split(" ")[1].split(":")[2]);
+
+    secondsPointer.rotation.z = -(seconds / 60) * 2 * Math.PI;
+    minutePointer.rotation.z = -(minutes / 60) * 2 * Math.PI;
+    hourPointer.rotation.z = -(hours / 12 + minutes / 720) * 2 * Math.PI;
 
     return <primitive object={scene} scale={1.1} />;
 };
 
 const ModelViewer = () => {
     return (
-        <Suspense fallback={<div className="w-full h-full flex flex-col gap-2 justify-center items-center">Loading...</div>}>
-            <Canvas camera={{ position: [-10, 0, 0], fov: 50 }} gl={{ antialias: true, powerPreference: "high-performance" }} dpr={[1, 2]}>
+        <Suspense
+            fallback={
+                <div className="w-full h-full flex flex-col gap-2 justify-center items-center">
+                    Loading...
+                </div>
+            }
+        >
+            <Canvas
+                camera={{ position: [-10, 0, 0], fov: 50 }}
+                gl={{ antialias: true, powerPreference: "high-performance" }}
+                dpr={[1, 2]}
+            >
                 <ambientLight intensity={0.5} />
                 <directionalLight position={[10, 10, 5]} intensity={1} />
                 <directionalLight position={[-10, 10, 5]} intensity={0.5} />
@@ -36,4 +58,3 @@ const ModelViewer = () => {
 };
 
 export default ModelViewer;
-
