bdcfe44935e0b3fb390d5d1709ad85725d2afcf9
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-02-23 21:19:59 -0500

    fix: fix data fetching error

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,5 +1,7 @@
 export const useGetData = async (endpoint) => {
-    const data = await fetch(process.env.APP_DOMAIN + endpoint);
+    const data = await fetch(
+        (process.env.APP_DOMAIN || "https://tempus-elite.onrender.com") + endpoint
+    );
     const response = await data.json();
 
     return response.data;
